4f9720cb36e39f36078987c4d5b19e751c3ae93e
Franz Schreiner <grunztiger@gmail.com>
2013-03-27 17:32:19 +0100

    Receivermap for each program.

--- catroid/src/org/catrobat/catroid/common/MessageContainer.java
+++ catroid/src/org/catrobat/catroid/common/MessageContainer.java
@@ -35,21 +35,24 @@ import android.widget.ArrayAdapter;
 
 public class MessageContainer {
 
-	private static TreeMap<String, Vector<BroadcastScript>> globalreceiverMap = new TreeMap<String, Vector<BroadcastScript>>();
-	//	private static ArrayAdapter<String> globalMessageAdapter = null;
-	private static TreeMap<String, ArrayAdapter<String>> projectMap = new TreeMap<String, ArrayAdapter<String>>();
+	private static TreeMap<String, TreeMap<String, Vector<BroadcastScript>>> programReceiverMap = new TreeMap<String, TreeMap<String, Vector<BroadcastScript>>>();
+	private static TreeMap<String, ArrayAdapter<String>> programMap = new TreeMap<String, ArrayAdapter<String>>();
 
 	public static void clear() {
-		globalreceiverMap.clear();
-		projectMap.clear();
+		programReceiverMap.clear();
+		programMap.clear();
 	}
 
 	public static void addMessage(String message) {
 		if (message.length() == 0) {
 			return;
 		}
-		if (!globalreceiverMap.containsKey(message)) {
-			globalreceiverMap.put(message, new Vector<BroadcastScript>());
+		String currentProjectName = ProjectManager.getInstance().getCurrentProject().getName();
+		if (programReceiverMap.get(currentProjectName) == null) {
+			programReceiverMap.put(currentProjectName, new TreeMap<String, Vector<BroadcastScript>>());
+		}
+		if (!programReceiverMap.get(currentProjectName).containsKey(message)) {
+			programReceiverMap.get(currentProjectName).put(message, new Vector<BroadcastScript>());
 			addMessageToAdapter(message);
 		}
 	}
@@ -58,64 +61,69 @@ public class MessageContainer {
 		if (message.length() == 0) {
 			return;
 		}
-		if (globalreceiverMap.containsKey(message)) {
-			globalreceiverMap.get(message).add(script);
+
+		String currentProjectName = ProjectManager.getInstance().getCurrentProject().getName();
+		if (programReceiverMap.get(currentProjectName).containsKey(message)) {
+			programReceiverMap.get(currentProjectName).get(message).add(script);
 		} else {
 			Vector<BroadcastScript> receiverVec = new Vector<BroadcastScript>();
 			receiverVec.add(script);
-			globalreceiverMap.put(message, receiverVec);
+			programReceiverMap.get(currentProjectName).put(message, receiverVec);
 			addMessageToAdapter(message);
 		}
 	}
 
 	public static void deleteReceiverScript(String message, BroadcastScript script) {
-		if (globalreceiverMap.containsKey(message)) {
-			globalreceiverMap.get(message).removeElement(script);
+		String currentProjectName = ProjectManager.getInstance().getCurrentProject().getName();
+		if (programReceiverMap.get(currentProjectName).containsKey(message)) {
+			programReceiverMap.get(currentProjectName).get(message).removeElement(script);
 		}
 	}
 
 	public static Vector<BroadcastScript> getReceiverOfMessage(String message) {
-		return globalreceiverMap.get(message);
+		return programReceiverMap.get(ProjectManager.getInstance().getCurrentProject().getName()).get(message);
 	}
 
 	public static Set<String> getMessages() {
-		return globalreceiverMap.keySet();
+		return programReceiverMap.get(ProjectManager.getInstance().getCurrentProject()).keySet();
 	}
 
 	private static synchronized void addMessageToAdapter(String message) {
-		ArrayAdapter<String> projectMessageAdapter = projectMap.get(ProjectManager.getInstance().getCurrentProject()
+		ArrayAdapter<String> projectMessageAdapter = programMap.get(ProjectManager.getInstance().getCurrentProject()
 				.getName());
 		if (projectMessageAdapter != null) {
 			projectMessageAdapter.add(message);
 		}
-		projectMap.put(ProjectManager.getInstance().getCurrentProject().getName(), projectMessageAdapter);
+		programMap.put(ProjectManager.getInstance().getCurrentProject().getName(), projectMessageAdapter);
 	}
 
 	public static synchronized ArrayAdapter<String> getMessageAdapter(Context context) {
-		ArrayAdapter<String> projectMessageAdapter = projectMap.get(ProjectManager.getInstance().getCurrentProject()
-				.getName());
+		String currentProjectName = ProjectManager.getInstance().getCurrentProject().getName();
+		ArrayAdapter<String> projectMessageAdapter = programMap.get(currentProjectName);
 		if (projectMessageAdapter == null) {
 			projectMessageAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item);
 			projectMessageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 			projectMessageAdapter.add(context.getString(R.string.broadcast_nothing_selected));
-			Set<String> messageSet = globalreceiverMap.keySet();
-			for (String message : messageSet) {
-				projectMessageAdapter.add(message);
+			if (programReceiverMap.get(currentProjectName) != null) {
+				Set<String> messageSet = programReceiverMap.get(currentProjectName).keySet();
+				for (String message : messageSet) {
+					projectMessageAdapter.add(message);
+				}
 			}
 		}
 		return projectMessageAdapter;
 	}
 
 	public static int getPositionOfMessageInAdapter(String message) {
-		if (!globalreceiverMap.containsKey(message)) {
+		String currentProjectName = ProjectManager.getInstance().getCurrentProject().getName();
+		ArrayAdapter<String> projectMessageAdapter = programMap.get(currentProjectName);
+		if (projectMessageAdapter == null || programReceiverMap.get(currentProjectName) == null) {
+			return -1;
+		}
+		if (!programReceiverMap.get(currentProjectName).containsKey(message)) {
 			return -1;
 		}
 
-		ArrayAdapter<String> projectMessageAdapter = projectMap.get(ProjectManager.getInstance().getCurrentProject()
-				.getName());
-		if (projectMessageAdapter == null) {
-			return -1;
-		}
 		return projectMessageAdapter.getPosition(message);
 	}
 }
